d9da348c0568d11ebdfe19400b3cab5fe3fa7332
reneelane0 <w0494485@campus.nscc.ca>
2025-01-22 12:29:10 -0400

    created discussion and comment models with nav properties

---
+++ Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace WildlifeForum.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; } // primary key
+        public string Content { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int DiscussionId { get; set; } // foreign key
+
+        // navigation property
+        public Discussion? Discussion { get; set; }
+
+    }
+}
---
+++ Models/Discussion.cs
@@ -0,0 +1,16 @@
+﻿namespace WildlifeForum.Models
+{
+    public class Discussion
+    {
+        public int DiscussionId { get; set; } // primary key
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; }
+        public string ImageFilename { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        // navigation property
+        public List<Comment>? Comments { get; set; }
+
+    }
+}
